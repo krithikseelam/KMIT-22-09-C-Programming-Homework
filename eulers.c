2f85344082a1c078416bd1e29ded038448dd0882
krithik seelam <krithikseelam9999@gmail.com>
2026-09-23 21:07:17 +0530

    Added Eulers series question

---
+++ eulers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int n, i = 0;
+	float sum = 0;
+	float fact = 1;
+
+        printf("Enter the number of terms for which you want eulers to calculate: ");
+        scanf("%d", &n);
+
+        while (i < n) {
+        if (i > 0){
+            fact = fact * i;
+        }
+
+        sum = sum + (1.0 / fact);
+        i++;
+        }
+	printf("Euler's value = %f", sum);
+	return 0;
+}
